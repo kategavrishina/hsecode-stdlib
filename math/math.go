20a2ec07cc39972bb103f0387848cc9805b859b7
Katya Gavrishina <9katenok9@gmail.com>
2020-03-03 15:01:42 +0300

    Add NthPrime func

--- math/math.go
+++ math/math.go
@@ -5,30 +5,29 @@ import (
 )
 
 func NthPrime(n int) int {
-
 	if n < 1 {
 		panic("n < 1 error")
 	}
+	if n == 1 {
+		return 2
+	}
 	edge := 2 * (math.Floor(float64(n)*math.Log(float64(n))) + 1)
-	integers := make([]bool, int(edge)+2)
-	for i := 2; i < int(edge)+1; i++ {
+	integers := make([]bool, int(edge)/2)
+	for i := 0; i < len(integers); i++ {
 		integers[i] = true
 	}
-
-	for p := 2; p*p <= int(edge); p++ {
-		if integers[p] == true {
-			for i := p * 2; i <= int(edge); i += p {
-				integers[i] = false
+	for p := 3; p*p <= len(integers)*2; p += 2 {
+		if integers[((p+1)/2)-2] == true {
+			for i := p * 3; i <= (len(integers)*2)+1; i += 2 * p {
+				integers[int(math.Floor(float64(i/2)))-1] = false
 			}
 		}
 	}
-
 	var primes []int
-	for p := 2; p <= int(edge); p++ {
+	for p := 0; p < len(integers); p++ {
 		if integers[p] == true {
-			primes = append(primes, p)
+			primes = append(primes, (p*2)+3)
 		}
 	}
-
-	return primes[n-1]
+	return primes[n-2]
 }
