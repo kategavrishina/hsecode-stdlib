92246d7de7024df2fa1a81c94b952ad5e944f7db
Katya Gavrishina <9katenok9@gmail.com>
2020-03-11 12:52:00 +0300

    Add primes func

--- math/math.go
+++ math/math.go
@@ -4,12 +4,20 @@ import (
 	"math"
 )
 
+var primes = [70]int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+	31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+	73, 79, 83, 89, 97, 101, 103, 107, 109, 113,
+	127, 131, 137, 139, 149, 151, 157, 163, 167, 173,
+	179, 181, 191, 193, 197, 199, 211, 223, 227, 229,
+	233, 239, 241, 251, 257, 263, 269, 271, 277, 281,
+	283, 283, 293, 307, 311, 313, 317, 331, 337, 347}
+
 func NthPrime(n int) int {
 	if n < 1 {
 		panic("n < 1 error")
 	}
-	if n == 1 {
-		return 2
+	if n <= 70 {
+		return primes[n-1]
 	}
 	edge := math.Ceil(float64(n) * (math.Log(float64(n)) + math.Log(math.Log(float64(n)))))
 	integers := make([]bool, int(float64(int(edge)/2)))
