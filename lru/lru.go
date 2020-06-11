8c68634d7a5d688176d30f4a0315ed1de81b46c8
Katya Gavrishina <9katenok9@gmail.com>
2020-06-11 23:05:59 +0300

    Add lru package

--- lru/lru.go
+++ lru/lru.go
@@ -1,1 +1,52 @@
 package lru
+
+import "container/list"
+
+type Cache struct {
+	// contains filtered or unexported fields
+	elements *list.List
+	hash     map[int]*list.Element
+	cap      int
+}
+
+type Pair struct {
+	key, value int
+}
+
+func New(capacity int) *Cache {
+	c := new(Cache)
+	c.elements = list.New()
+	c.cap = capacity
+	c.hash = make(map[int]*list.Element)
+
+	return c
+}
+
+func (cache *Cache) Get(key int) (int, bool) {
+	elem, ok := cache.hash[key]
+	if !ok {
+		return 0, false
+	} else {
+		cache.elements.MoveToFront(elem)
+		val := elem.Value.(Pair)
+		return val.value, true
+	}
+}
+
+func (cache *Cache) Put(key int, value int) {
+	elem, ok := cache.hash[key]
+	if !ok {
+		if cache.elements.Len() == cache.cap {
+			el := cache.elements.Back()
+			delete(cache.hash, el.Value.(Pair).key)
+			cache.elements.Remove(el)
+			cache.elements.PushFront(Pair{key, value})
+			cache.hash[key] = cache.elements.Front()
+		} else {
+			cache.elements.PushFront(Pair{key, value})
+			cache.hash[key] = cache.elements.Front()
+		}
+	} else {
+		cache.elements.MoveToFront(elem)
+	}
+}
--- lru/lru_test.go
+++ lru/lru_test.go
@@ -1,1 +1,62 @@
-package lru
+package lru_test
+
+import (
+	"hsecode.com/stdlib/lru"
+	"testing"
+)
+
+func TestEasyGetLRU(t *testing.T) {
+	c := lru.New(3) // cache of size 3
+	c.Put(1, 100)
+	c.Put(2, 200)
+	c.Put(3, 300)
+	ansInt, ansBool := c.Get(1)
+	if ansInt != 100 {
+		t.Fatalf("Get existing key doesn't work: value %d", ansInt)
+	}
+	if ansBool != true {
+		t.Fatalf("Get existing key doesn't work: bool %v", ansBool)
+	}
+	ansInt1, ansBool1 := c.Get(5)
+	if ansInt1 != 0 {
+		t.Fatalf("Get not existing key doesn't work: value %d", ansInt1)
+	}
+	if ansBool1 != false {
+		t.Fatalf("Get not existing key doesn't work: bool %v", ansBool1)
+	}
+}
+
+func TestGetPutLRU(t *testing.T) {
+	c := lru.New(2) // cache of size 2
+
+	c.Put(1, 100)
+	c.Put(2, 200)
+	c.Put(3, 300)
+	ansInt, ansBool := c.Get(1)
+	if ansInt != 0 {
+		t.Fatalf("Get removed key doesn't work: value %d", ansInt)
+	}
+	if ansBool != false {
+		t.Fatalf("Get removed key doesn't work: bool %v", ansBool)
+	}
+
+	c.Get(2)      // "use" the key
+	c.Put(4, 400) // remove `3` because `2` was used recently.
+	c.Put(2, 200) // "use" `2`
+	c.Put(5, 500) // remove `4`, `2` is still there
+	ansInt1, ansBool1 := c.Get(2)
+	if ansInt1 != 200 {
+		t.Fatalf("Get recently used key doesn't work: value %d", ansInt1)
+	}
+	if ansBool1 != true {
+		t.Fatalf("Get recently used key doesn't work: bool %v", ansBool1)
+	}
+
+	ansInt2, ansBool2 := c.Get(4)
+	if ansInt2 != 0 {
+		t.Fatalf("Get recently removed key doesn't work: value %d", ansInt2)
+	}
+	if ansBool2 != false {
+		t.Fatalf("Get recently removed key doesn't work: bool %v", ansBool2)
+	}
+}
