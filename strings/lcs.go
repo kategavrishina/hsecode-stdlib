1f0cdab70499e44e42899d7f5e618887bf47ae72
Katya Gavrishina <9katenok9@gmail.com>
2020-06-26 11:38:40 +0300

    Add lcs

--- strings/lcs.go
+++ strings/lcs.go
@@ -11,14 +11,10 @@ func LCS(s1, s2 string) string {
 	n := len(s2)
 	C := matrix.New(m+1, n+1)
 	for i := 0; i < m+1; i++ {
-		C.Set(i, 0, 0)
-	}
-	for j := 0; j < n+1; j++ {
-		C.Set(0, j, 0)
-	}
-	for i := 1; i < m+1; i++ {
-		for j := 1; j < n+1; j++ {
-			if s1[i-1] == s2[j-1] {
+		for j := 0; j < n+1; j++ {
+			if i == 0 || j == 0 {
+				C.Set(i, j, 0)
+			} else if s1[i-1] == s2[j-1] {
 				C.Set(i, j, C.Get(i-1, j-1)+1)
 			} else {
 				C.Set(i, j, int(math.Max(float64(C.Get(i, j-1)), float64(C.Get(i-1, j)))))
